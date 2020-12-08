f8b6d438b2692512c2eb0241777090656876f696
Keytrap <badniiw@gmail.com>
2020-12-08 23:08:40 +0100

    -

---
+++ Themes/Dark.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Keytrap.Theme.Themes
+{
+    public partial class Dark : ResourceDictionary
+    {
+        public void BtnMinimize_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BtnMaximize_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BtnClose_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GrdDragForm_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
